8540fec07f30b29b5916274d6ac05be78a894fce
Minjin <minjinsong@hotmail.com>
2016-02-25 19:25:06 +0900

    add resource4

---
+++ resource4/start_client_req_5a.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sleep 0.8
+./resource_client3 127.0.0.1 2048 0 1000 log5.txt 20 &
+
+sleep 4.5
+./resource_client3 127.0.0.1 2048 0 1000 log5.txt 20 &
+
+sleep 2.3
+./resource_client3 127.0.0.1 2048 0 1000 log5.txt 20 &
+
+sleep 1.75
+./resource_client3 127.0.0.1 2048 0 1000 log5.txt 20 &
+
+sleep 5.4
+./resource_client3 127.0.0.1 2048 0 1000 log5.txt 20 &
+
